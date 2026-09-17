6e9142a1460f465a6146ed7cee23502fd37c0900
Stephen Tims <stimsdesign@gmail.com>
2026-09-17 14:50:51 -0700

    feat: add automatic XML sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,44 +6,55 @@ import { defineConfig } from 'astro/config';
 // import { siteConfig } from './src/siteConfig';
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 // Export the Astro configuration https://astro.build/config
 export default defineConfig({
   // Sets default to static; use 'export const prerender = false;' in pages/APIs to opt-into SSR/Dynamic mode.
   output: 'static',
+
   // Host environment – Tells Astro how to build the site for specific platforms (Netlify, Node, etc.)
   adapter: netlify(),
+
   // Disable "Pretty URLs" in Netlify Dashboard.
   trailingSlash: 'never',
-    fonts: [
-        {
-            provider: fontProviders.google(),
-            name: 'Raleway',
-            cssVariable: '--font-raleway',
-            // weights: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-            weights: ['900']
-        },
-        {
-            provider: fontProviders.google(),
-            name: 'Quicksand',
-            cssVariable: '--font-quicksand',
-            // weights: ['300', '400', '500', '600', '700']
-            weights: ['400', '700']
-        },
-        {
-            provider: fontProviders.google(),
-            name: 'Roboto Condensed',
-            cssVariable: '--font-roboto-condensed',
-            weights: ['500','900']
-        },
-    ],  
-    build: {
-        format: 'file' // Output pages as standalone .html files (e.g., /services.html) to support clean URLs (/services)
-    },
-    server: {
-        host: true,   // or '0.0.0.0'
-        port: 4321,
-    },
-    devToolbar: {
-        enabled: false,
-    },
+
+  fonts: [
+      {
+          provider: fontProviders.google(),
+          name: 'Raleway',
+          cssVariable: '--font-raleway',
+          // weights: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+          weights: ['900']
+      },
+      {
+          provider: fontProviders.google(),
+          name: 'Quicksand',
+          cssVariable: '--font-quicksand',
+          // weights: ['300', '400', '500', '600', '700']
+          weights: ['400', '700']
+      },
+      {
+          provider: fontProviders.google(),
+          name: 'Roboto Condensed',
+          cssVariable: '--font-roboto-condensed',
+          weights: ['500','900']
+      },
+  ],
+
+  build: {
+      format: 'file' // Output pages as standalone .html files (e.g., /services.html) to support clean URLs (/services)
+  },
+
+  server: {
+      host: true,   // or '0.0.0.0'
+      port: 4321,
+  },
+
+  devToolbar: {
+      enabled: false,
+  },
+
+  site: 'https://qualitypoolandspa.com',
+  integrations: [sitemap()],
 });
